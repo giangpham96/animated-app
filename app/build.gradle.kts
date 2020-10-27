e0631017440bc094254495af42bff43b26556da2
Giang Pham <conmeobeo96@gmail.com>
2020-10-27 22:15:35 +0200

    Update dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdkVersion(30)
+
+    defaultConfig {
+        applicationId = "la.me.leo.animatedapp"
+        minSdkVersion(23)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(Dependencies.kotlinStdLib)
+    implementation(Dependencies.coreKtx)
+    implementation(Dependencies.appCompat)
+    implementation(Dependencies.constraintLayout)
+    implementation(Dependencies.materialDesign)
+    implementation(Dependencies.fragmentKtx)
+    implementation(Dependencies.transition)
+}
