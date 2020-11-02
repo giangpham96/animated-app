09ffcf1156c2aed288ea4c77b5d832e591267a0a
Giang Pham <conmeobeo96@gmail.com>
2020-11-02 23:07:01 +0200

    Finalize main tab push animation

--- app/src/main/java/la/me/leo/animatedapp/tabs/transition/MainTabsPushTransition.kt
+++ app/src/main/java/la/me/leo/animatedapp/tabs/transition/MainTabsPushTransition.kt
@@ -5,7 +5,11 @@ import android.animation.AnimatorSet
 import android.animation.ValueAnimator
 import android.view.View
 import android.view.ViewAnimationUtils
+import android.view.ViewGroup
 import androidx.core.view.animation.PathInterpolatorCompat
+import androidx.core.view.children
+import androidx.core.view.forEach
+import androidx.recyclerview.widget.RecyclerView
 import la.me.leo.animatedapp.R
 import la.me.leo.core.base.transition.FragmentTransition
 import kotlin.math.pow
@@ -18,7 +22,8 @@ internal class MainTabsPushTransition(private val x: Int, private val y: Int) :
         val translationY0 = fragmentRoot.context.resources.getDimensionPixelSize(R.dimen.u2)
         val radius0 = 0f
         val radius1 = sqrt(fragmentRoot.height.toFloat().pow(2f) + x.toFloat().pow(2f))
-        val animator1 = ViewAnimationUtils.createCircularReveal(fragmentRoot, x, y, radius0, radius1)
+        val animator1 =
+            ViewAnimationUtils.createCircularReveal(fragmentRoot, x, y, radius0, radius1)
         val animator2 = ValueAnimator.ofFloat(0f, 1f)
         animator2.addUpdateListener {
             val animatedValue = it.animatedValue as Float
@@ -27,8 +32,29 @@ internal class MainTabsPushTransition(private val x: Int, private val y: Int) :
                 translationY = (1 - animatedValue) * translationY0
             }
         }
+
+        val recyclerView =
+            (fragmentRoot as? ViewGroup)?.children?.firstOrNull { it is RecyclerView }
+        val recyclerViewItemAnimators = if (recyclerView is RecyclerView) {
+            val baseTranslationY0 = fragmentRoot.context.resources.getDimensionPixelSize(R.dimen.u1)
+            recyclerView.forEach { it.alpha = 0f }
+            recyclerView.children.mapIndexed { i, v ->
+                val Y0 = baseTranslationY0 + i * baseTranslationY0 * 2
+                ValueAnimator.ofFloat(0f, 1f).apply {
+                    addUpdateListener {
+                        val animatedValue = it.animatedValue as Float
+                        v.translationY = Y0 * (1 - animatedValue)
+                        v.alpha = 0.5f + 0.5f * animatedValue
+                    }
+                    startDelay = 50L * i
+                }
+            }.toList()
+        } else {
+            emptyList()
+        }
+
         val animatorSet = AnimatorSet()
-        animatorSet.playTogether(animator1, animator2)
+        animatorSet.playTogether(recyclerViewItemAnimators + listOf(animator1, animator2))
         animatorSet.interpolator = PathInterpolatorCompat.create(0.25f, 0.1f, 0.25f, 1f)
         animatorSet.duration = 300L
         return animatorSet
