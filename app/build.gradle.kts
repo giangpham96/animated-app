8cf338260e6b883c516555b846d9eb61ed7fd52f
Giang Pham <conmeobeo96@gmail.com>
2020-12-10 12:19:01 +0200

    Use single activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     implementation(Dependencies.materialDesign)
     implementation(Dependencies.fragmentKtx)
     implementation(Dependencies.transition)
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:${rootProject.extra["kotlin_version"]}")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
 }
--- app/src/main/java/la/me/leo/animatedapp/MainActivity.kt
+++ app/src/main/java/la/me/leo/animatedapp/MainActivity.kt
@@ -1,21 +1,21 @@
 package la.me.leo.animatedapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import la.me.leo.animatedapp.databinding.ActivityMainBinding
-import la.me.leo.animatedapp.tabs.MainTabsActivity
+import la.me.leo.animatedapp.tabs.TabsFragment
 
 internal class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        binding.btnTabsTransition.setOnClickListener {
-            startActivity(Intent(this, MainTabsActivity::class.java))
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                add(R.id.fragmentRoot, TabsFragment())
+            }
         }
     }
 }
